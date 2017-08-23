4ffe4fe2cd74d8d83c00cf6298b9bfb92fcd5af5
Teerasej <teerasej@gmail.com>
2017-08-23 16:49:09 +0700

    Use body-parser as middleware

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+let bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     console.log('Got request at', new Date());
